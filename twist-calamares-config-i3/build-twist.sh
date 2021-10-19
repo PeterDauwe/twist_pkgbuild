fd42a176a1006d4d156d784b8e45a2f50156b011
PeterDauwe <bobo5290461@gmail.com>
2021-10-19 07:42:17 +0200

    up

--- twist-calamares-config-i3/build-twist.sh
+++ twist-calamares-config-i3/build-twist.sh
@@ -1,42 +1,32 @@
 #!/bin/bash
 #https://wiki.archlinux.org/index.php/DeveloperWiki:Building_in_a_Clean_Chroot
+deletion1="../../twist_repo/x86_64/"
+name1="${PWD##*/}"
 
-destination1="../../twist_repo/x86_64/"
+rm $deletion1/$name1-21*.zst
 
-destiny=$destination1
 
-search1="twist-calamares"
-search=$search1
 
-tput setaf 2
-echo "#############################################################################################"
-echo "#########        Let us build the package "$(basename `pwd`)
-echo "#############################################################################################"
-tput sgr0
+destiny="../../twist_repo/x86_64"
+
+#destiny=$destination1
 
 makepkg
 
 echo "Moving created files to " $destiny
 echo "#############################################################################################"
 mv $search*pkg.tar.zst $destiny
+sh $destiny/update_repo.sh &
+ls | grep -v "PKGBUILD\|build-twist.sh" | xargs rm -rf
+
 echo "Cleaning up"
 echo "#############################################################################################"
 echo "deleting unnecessary folders"
 echo "#############################################################################################"
 
-rm -rf pkg src
-
-echo "deleting unnecessary files"
-echo "#############################################################################################"
-
-rm -r calamares-*
-
-
-
 cd $destiny
 
 sh ./update_repo.sh &
-
 
 tput setaf 8
 echo "#############################################################################################"
